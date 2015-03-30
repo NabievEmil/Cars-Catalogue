c8c741a54d8b6752ed915903480d9485e087cf58
Наталия <natalia@MacBook-Air-Natalia.local>
2015-03-30 16:17:56 +0300

    Initial commit

---
+++ Homework3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework3
+//
+//  Created by Наталия on 30.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
