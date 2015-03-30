5f84484611df50e0d5e8665afc86788dce5e4acc
Наталия <natalia@MacBook-Air-Natalia.local>
2015-03-30 18:55:42 +0300

    Work is done, but without constrains

---
+++ Homework3/CatalogueViewController.h
@@ -0,0 +1,16 @@
+//
+//  CatalogueViewController.h
+//  Homework3
+//
+//  Created by Наталия on 30.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatalogueViewController : UIViewController
+
+@property (strong,nonatomic) NSString *imageName;
+@property (strong,nonatomic) NSString *textFill;
+
+@end
